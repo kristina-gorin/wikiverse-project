8c3d329df8454893130d17efb88bc96069ba39d1
Kristina Gorina <kristina.gorina@kristina.gorina-MacBook-Pro>
2022-12-20 15:51:27 -0500

    on click functionality for article

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
+import apiURL from '../api';
+
+export const Article = (props)=>{
+  
+
+  return <div>
+    <h4>{props.article.author.name}</h4>
+    <h4>{props.article.createdAt}</h4>
+    <h4 className='content'>
+    {props.article.content}</h4>
+    <button>GO BACK</button>
+    
+    
+  </div>
+}
 
 export const Page = (props) => {
-
+  const [article, setArticle]= useState(undefined);
+  const handleClick = async()=>{
+    const res = await fetch(`${apiURL}/wiki/${props.page.slug}`)
+    const data = await res.json();
+    console.log(data)
+    setArticle(data)
+  }
+  
   return <>
-    <h3>{props.page.title}</h3>
+    <h3 className = 'headline' 
+    onClick={handleClick}>{props.page.title}</h3>
+    <div className='article'>
+      
+      {
+        article ? <Article article = {article}/> : ""
+      }
+    </div>
+    
   </>
 } 
 	
