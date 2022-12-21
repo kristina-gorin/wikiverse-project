3e2f272f719e1465d990a15ce02ba735c2fc0b60
Kristina Gorina <kristina.gorina@kristina.gorina-MacBook-Pro>
2022-12-20 19:29:19 -0500

    final commit, wip

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -6,13 +6,40 @@ export const Article = (props)=>{
   const date = props.article.createdAt.slice(0,10)
   return <div>
     <h4>{props.article.author.name}</h4>
-    <h4>{date}</h4>
+
     <h4 className='content'>
     {props.article.content}</h4>
+    <h6>{props.article.tags.map((tag)=>"#"+tag.name)}</h6>
+    <h4>{date}</h4>
     <button onClick = {()=>{props.setArticle(undefined)}}>GO BACK</button>
     
     
   </div>
+}
+export const NewArticleForm = ()=>{
+  const[title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail]= useState('');
+
+  const handleSubmit = async (event)=>{
+    event.preventDefault();
+    
+
+    
+    setTitle('');
+    setContent('');
+    setName('');
+    setEmail('');
+  }
+
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input
+      />
+    </form>
+  )
 }
 
 export const Page = (props) => {
